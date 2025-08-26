451892e9cc6c94246ad4d49afd55ee960ff7fdab
IamMax279 <maksssiula@gmail.com>
2025-08-26 21:43:38 +0200

    include all the features for a potentially better prediction score

--- src/features/feature_extraction.py
+++ src/features/feature_extraction.py
@@ -2,13 +2,16 @@ from src.data.load_data import load_housing_data
 
 def get_housing_features():
     df = load_housing_data()
-    
-    # Select multiple columns from the data frame - hence the nested list
-    feature_columns = df[
-        ['housing_median_age',
-        'median_income',
-        'population_per_house',
-        'rooms_per_house']
-    ]
 
-    return feature_columns
+    feature_data = df[[
+        'HouseAge',
+        'AveRooms',
+        'AveBedrms',
+        'AveOccup',
+        'MedInc',
+        'Population',
+        'Latitude',
+        'Longitude'
+    ]]
+    
+    return feature_data
