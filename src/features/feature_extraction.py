6c4600cf723265b7c55b30445b0c70323e861cb2
IamMax279 <maksssiula@gmail.com>
2025-08-28 20:52:13 +0200

    add patients dataset feature extractor function

--- src/features/feature_extraction.py
+++ src/features/feature_extraction.py
@@ -1,4 +1,4 @@
-from src.data.load_data import load_housing_data
+from src.data.load_data import load_housing_data, load_diabetes_data
 
 def get_housing_features():
     df = load_housing_data()
@@ -14,4 +14,32 @@ def get_housing_features():
         'Longitude'
     ]]
     
+    return feature_data
+
+def get_patients_features():
+    df = load_diabetes_data()
+
+    df['gender'] = df['gender'].apply(lambda x: 1 if x == 'Male' else 0)
+
+    mapping = {
+        'No Info': 0,
+        'current': 1,
+        'ever': 2,
+        'former': 3,
+        'never': 4,
+        'not current': 5,
+    }
+    df['smoking_history'] = df['smoking_history'].map(mapping)
+
+    feature_data = df[[
+        'gender',
+        'age',
+        'hypertension',
+        'heart_disease',
+        'smoking_history',
+        'bmi',
+        'HbA1c_level',
+        'blood_glucose_level'
+    ]]
+
     return feature_data
