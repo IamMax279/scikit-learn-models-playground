c6e8defcae713bb6371a06bb259857a1013cdc63
IamMax279 <maksssiula@gmail.com>
2025-08-29 20:51:28 +0200

    add a confusion matrix visualization to the logistic regression model file

--- src/models/train_logistic_regression.py
+++ src/models/train_logistic_regression.py
@@ -2,6 +2,7 @@ from src.features.feature_extraction import get_patients_features
 from src.data.load_data import load_diabetes_data
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -20,20 +21,25 @@ def train_logistic_regression():
     score = model.score(X_test, y_test)
     print('score:', score)
 
-    plt.figure(figsize=(10, 6))
+    # In this case, the graphs look a bit odd since the LR model assigns binary values
 
-    sns.scatterplot(x=X_test['blood_glucose_level'], y=y_test, color='blue')
-    sns.scatterplot(x=X_test['blood_glucose_level'], y=y_pred, color='red')
-    plt.xlabel('Actual diabetes outcome')
-    plt.ylabel('Predicted diabetes outcome')
-    plt.title('Data vs predictions')
-
+    cm = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    # insert possible targets (0 - healthy, 1 - diabetic)
+    disp = ConfusionMatrixDisplay(cm, display_labels=[0, 1])
+    # set color map shades for color blue
+    disp.plot(cmap='Blues')
+    plt.title('Confusion matrix')
     plt.show()
 
     plt.figure(figsize=(10, 6))
-    sns.histplot(residuals, bins=15, kde=True)
-    plt.xlabel('Residuals')
-    plt.ylabel('Frequency')
+
+    sns.scatterplot(x=y_test, y=y_pred, color='blue')
+    plt.xlabel('Actual diabetes outcome')
+    plt.ylabel('Predicted diabetes outcome')
+    plt.title('Data vs predictions')
+    plt.xticks([0, 1])
+    plt.yticks([0, 1])
+    plt.grid(True)
 
     plt.show()
 
